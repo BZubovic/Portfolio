d29412136a87920e76279e449db6217e430dc7d3
Benjamin <your_email@example.combenjamin.zubovic1@gmail.com>
2021-05-05 12:25:39 +0200

    updates

---
+++ form.php
@@ -0,0 +1,12 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") { // checks whether a POST request actually exists or not.
+
+    $user_subject = strip_tags(trim($_POST['subject']));
+    $user_message = strip_tags(trim($_POST['message']));
+
+    $send_to = 'bzubovic@outlook.com';
+
+    mail($send_to, $user_subject, $subject_message);
+
+}
