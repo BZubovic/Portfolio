cfcfb5535a8e4815f87b46d23c7ceee4e0a02df8
Benjamin <your_email@example.combenjamin.zubovic1@gmail.com>
2021-05-13 16:24:16 +0200

    added about page

---
+++ js/about.js
@@ -0,0 +1,62 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+   /* variables */
+ 
+   const menu= document.querySelector(".menu-icon");
+   const menucolor=document.querySelectorAll(".menu-icon span")
+   const mobilemenu = document.querySelector(".mobile-menu");
+   const logo =document.querySelectorAll(".logo svg path");
+   const mobile_menu_item=document.querySelectorAll(".mobile-menu li a");
+    
+   /* menu */
+   menu.addEventListener("click", toggle_menu);
+
+   function toggle_menu() {
+    menu.classList.toggle("open");
+    mobilemenu.classList.toggle("mobile-menu-open");
+     logo.forEach(function(element, index){
+         if(element.classList.contains("logo-color")){
+             setTimeout(() => {
+                element.classList.toggle("logo-color");
+             }, 300);
+         }else{
+            element.classList.toggle("logo-color");
+         }
+        
+    }); 
+    menucolor.forEach(function(element, index){
+        if(element.classList.contains("menu-color")){
+            setTimeout(() => {
+               element.classList.toggle("menu-color");
+            }, 300);
+        }else{
+           element.classList.toggle("menu-color");
+        }
+       
+   }); 
+   mobile_menu_item.forEach(function(element, index){
+    element.classList.toggle("show");
+   
+}); 
+mobile_menu_item.forEach(function(element, index){
+    element.addEventListener("click",toggle_menu);
+   
+}); 
+    
+
+
+}
+/* /menu */
+
+
+
+
+
+
+
+
+
+
+
+});
+
